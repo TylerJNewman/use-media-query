31604c0c32936434a2294d98a0dbb6ec124a708d
Tyler Newman <tylerjnewman18@gmail.com>
2020-07-03 11:53:29 -0700

    Add hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,44 @@
-import * as React from 'react';
+import { useState, useEffect } from "react";
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState<{
-    counter: number;
-  }>({
-    counter: 0
-  });
+interface MediaState {
+  isMobile?: boolean;
+  isTablet?: boolean;
+  isDesktop?: boolean;
+}
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++;
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval);
-    };
+const MOBILE_BREAKPOINT = 768;
+const TABLET_BREAKPOINT = 992;
+
+const getState = () => {
+  if (typeof window === "undefined") {
+    return { isMobile: null, isTablet: null, isDesktop: null };
+  }
+
+  const { innerWidth } = window;
+  const isMobile = innerWidth < MOBILE_BREAKPOINT;
+  const isTablet = !isMobile && innerWidth < TABLET_BREAKPOINT;
+  const isDesktop = !isMobile && !isTablet;
+
+  const mediaState: MediaState = {
+    isMobile,
+    isTablet,
+    isDesktop,
+  };
+
+  return mediaState;
+};
+
+const useMediaQuery = (): MediaState => {
+  const [state, setState] = useState({});
+  const onResize = () => setState(getState());
+
+  useEffect(() => {
+    onResize();
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
   }, []);
 
-  return counter;
+  return state;
 };
+
+export default useMediaQuery;
